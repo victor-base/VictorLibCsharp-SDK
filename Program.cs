cce19b557937a1f89564ccd9f7e4e363b5af68f7
Ivan Rodriguez. <ivanrwcm25@gmail.com>
2025-04-06 16:36:39 -0300

    New License

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
-// Copyright (C) 2025 Iván E. Rodríguez. Contact: ivanrwcm25@gmail.com
-//
-// This file is part of the Victor Base binding for .NET.
-// The original Victor Base core project was created by Emiliano Billi (email: emiliano.billi@gmail.com).
-//
-// This program is free software: you can redistribute it and/or modify  
-// it under the terms of the GNU General Public License as published  
-// by the Free Software Foundation, either version 3 of the License,  
-// or (at your option) any later version.  
-//
-// This program is distributed in the hope that it will be useful,  
-// but WITHOUT ANY WARRANTY; without even the implied warranty of  
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
-// GNU General Public License for more details.  
-//
-// You should have received a copy of the GNU General Public License  
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+ * Victor Base SDK - Developed by Iván E. Rodriguez
+ * Based on the vector database core created by Emiliano A. Billi.
+ * 
+ * Copyright (C) 2025 Iván E. Rodriguez
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 
 using LibTest;
 using TestFunc;
